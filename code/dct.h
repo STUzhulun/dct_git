d7b3d6bbafeffc21f830b4a149c57e07a7288437
zhunlun_ubuntu <15zlyang3@stu.edu.cn>
2018-02-07 18:43:57 +0800

    finish origin simple one

---
+++ code/dct.h
@@ -0,0 +1,10 @@
+#ifndef _DCT_H_
+#define _DCT_H_
+
+#include "matrix.h"
+#include <math.h>
+#define N 8
+
+void dct(const p_mat, p_mat);
+
+#endif
--- code/main.c
+++ code/main.c
@@ -3,20 +3,9 @@
 
 int  main(int argc ,char* argv[]) 
 {
-  p_mat s[4], res, x, mid;
+  p_mat res, x;
   int num;
-  FILE *fp_s[4], *fp_input;
-  char file_name[64] = {0};
-  for(num = 0; num < 4; num++)
-  {
-    sprintf(file_name, "matrix_s%d", num+1);
-    if((fp_s[num] = fopen(file_name,"r")) == NULL)
-    {
-      printf("Can't open the \"matrix_s%d\" file.\n",num+1);
-      exit(1);
-    }
-  }
-  
+  FILE *fp_input;
   printf("The sequence x[n] is followed...\n");
   if((fp_input = fopen("input","r")) == NULL)
   {
@@ -34,30 +23,10 @@ int  main(int argc ,char* argv[])
     printf("can't read matrix form file.\n");
     exit(1);
   }
-  
-  mid = createMat(N, 1);
+
   res = createMat(N, 1);
-  
-  for(num = 0; num < 4; num++)
-  {
-    s[num] = createMat(N, N);
-    if(readMat(fp_s[num], s[num]))
-    {
-      //showMat(s[num]);
-      fclose(fp_s[num]);
-    }
-    else
-    {
-      printf("can't read matrix form file.\n");
-      exit(1);
-    }
-  }
   dct(x,res);
-  /*
-  multiply(s[0], x, mid);
-  multiply(s[1], mid, res);
-  multiply(s[2], res, mid);
-  multiply(s[3], mid, res);*/
+
   printf("Its DCT trasnsform X[k] is followed...\n");
   showMat(res);
   return 0;
