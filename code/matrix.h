bb62fc669345be9ef5a274c0598d42fe86d4013e
zhunlun_ubuntu <15zlyang3@stu.edu.cn>
2018-02-07 18:47:37 +0800

    add a ignored file matrix.h

--- code/matrix.h
+++ code/matrix.h
@@ -1,6 +1,5 @@
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
-#define N 8
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,6 +17,6 @@ void Init(p_mat);
 char readMat(FILE *, p_mat);
 void showMat(p_mat);
 void multiply(const p_mat, const p_mat, p_mat);
-void dct(const p_mat, p_mat);
+
 
 #endif
